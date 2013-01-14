9c1bf79da72d95106fe90bf86b6bffbf75f70263
Mads Winther <winther.wander@gmail.com>
2013-01-14 11:58:29 +0100

    graph started

---
+++ Custom libs/graphics/graph.c
@@ -0,0 +1,66 @@
+#include "graph.h"
+
+
+// Initialize the graph window
+Graph * Graph_init(int x0, int y0, int width, int height) {
+
+	Graph * graph = (Graph *)malloc(sizeof(Graph));
+	
+	graph->x0 = x0;
+	graph->y0 = y0;
+	graph->width = width;
+	graph->height = height;
+	
+	graph->axis_x0 = x0 + MARGIN_LEFT;
+	graph->axus_y0 = y0 + MARGIN_TOP;
+	graph->axis_width = width - MARGIN_LEFT - MARGIN_RIGHT;
+	graph->axis_height = graph->height - MARGIN_TOP - MARGIN_BOTTOM;
+	
+	return graph;
+}
+
+void Graph_setXscale(Graph * graph, double x_min, double x_max) {
+	graph->x_min = x_min;
+	graph->x_max = x_max;
+}
+
+void Graph_setYscale(Graph * graph, double y_min, double y_max) {
+
+	graph->y_min = y_min;
+	graph->y_max = y_max;
+}
+
+
+void Graph_drawAxisNumber(Graph * graph, double number, double pos) {
+	
+	int x0, y0, x1, y1;
+	
+	if (pos > 100) pos = 100;
+	if (pos < 0) pos = 0;
+	
+	// Calculate text position and window dimensions
+	x0 = graph->x0 + 2;
+	y0 = (int)(graph->axis_y0 + graph->axis_height - (pos / 100.0) * axis_height);
+	x1 = x0 + Terminal_9_12_6.H_size * 6;
+	y1 = y0 + Terminal_9_12_6.V_size;
+	
+	GLCD_SetWindow(x0,y0,x1,y1);
+	GLCD_print(%3.2f, number);
+}
+
+
+void Graph_draw(Graph * graph) {
+	
+	// Draw background of the graph
+	drawFilledRectangle(graph->axis_x0, graph->axis_y0,
+						graph->axis_width, graph->axis_height, 
+						GRAPH_BACK_COLOR, GRAPH_STROKE_COLOR, 1);
+	
+	// Draw vertical axis numbers
+	Graph_drawAxisNumber(graph, graph->y_max, 100.0);
+	Graph_drawAxisNumber(graph, 0.5 * (graph->y_max - graph->y_min), 0.0);
+	Graph_drawAxisNumber(graph, graph->y_min, 0.0);
+	
+	
+}
+
---
+++ Custom libs/graphics/graph.h
@@ -0,0 +1,27 @@
+#define GRAPH_BACK_COLOR 	0xFFFFFF
+#define GRAPH_LINE_COLOR 	0x0000FF;
+#define GRAPH_STROKE_COLOR	0x000000;
+
+#define MARGIN_TOP			5
+#define MARGIN_BOTTOM		10
+#define MARGIN_LEFT			20
+#define MARGIN_RIGHT		0
+
+typedef struct {
+	int x0, y0;
+	int width, height;
+	double x_min, x_max;
+	double y_min, y_max;
+	
+	int axis_x0, axis_y0;
+	int axis_width, axis_height;
+} Graph;
+
+
+Graph * Graph_init(int x0, int y0, int width, int height);
+
+void Graph_setXscale(Graph * graph, double x_min, double x_max);
+void Graph_setYscale(Graph * graph, double y_min, double y_max);
+void Graph_drawAxisNumber(Graph * graph, double number, double pos);
+void Graph_draw(Graph * graph);
+
